d21382e3e4801a0c7f78023d932d568c5c53c645
mendesfsweden <mendesf@hotmail.com>
2020-10-04 21:37:19 +0000

    added docstrings to all functions in the app.py file

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+# Global Variables
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'FoodLibrary'
@@ -11,21 +12,43 @@ app.config['MONGO_URI'] = os.getenv('MONGO_URI', 'mongodb://localhost')
 mongo = PyMongo(app)
 page_limit = 6
 initial_page = 1
+home_page_limit = 3
 
 @app.route('/')
 def home():
-    recently_added_recipes=mongo.db.recipes.find().sort([("date",-1)]).limit(3)
-    most_viewed_recipes=mongo.db.recipes.find().sort([("clicks",-1)]).limit(3)
+    """
+    Renders the web application home page
+
+    Returns:
+        render_template("home.html",...): The home page template
+    """
+    recently_added_recipes=mongo.db.recipes.find().sort([("date",-1)]).limit(home_page_limit)
+    most_viewed_recipes=mongo.db.recipes.find().sort([("clicks",-1)]).limit(home_page_limit)
     return render_template("home.html", recently_added_recipes=recently_added_recipes, most_viewed_recipes=most_viewed_recipes)
     
 
 @app.route('/recipe/<recipe_id>')
 def get_recipe(recipe_id):
+    """
+    Renders the web application recipe page for the specified recipe
+
+    Parameters:
+        recipe_id (str): The id of the recipe to be rendered
+
+    Returns:
+        render_template("recipe.html",...): The recipe page template
+    """
     mongo.db.recipes.update_one({'_id': ObjectId(recipe_id)}, {"$inc":{"clicks": 1}})
     return render_template('recipe.html', recipe=mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)}))
 
 @app.route('/recipes')
 def get_recipes():
+    """
+    Renders the web application recipe list page
+
+    Returns:
+        render_template("recipes.html",...): The recipe list page template
+    """
     search=request.args.get('search')
     page = request.args.get('page')
     if not page:
@@ -45,10 +68,23 @@ def get_recipes():
 
 @app.route('/add_recipe')
 def add_recipe():
+    """
+    Renders the web application page to add a new recipe
+
+    Returns:
+        render_template("add_recipe.html",...): The add recipe page template
+    """
     return render_template('add_recipe.html', cuisines=mongo.db.cuisines.find().sort([("name", 1)]))
 
 @app.route('/insert_recipe', methods=["POST"])
 def insert_recipe():
+    """
+    Inserts a new recipe into the database and redirects 
+    the web application to the recipe list page
+
+    Returns:
+        redirect(url_for("get_recipes")): Redirects to the recipe list page
+    """
     recipe=request.form.to_dict()
     recipe["date"]=datetime.datetime.now()
     mongo.db.recipes.insert_one(recipe)
@@ -56,15 +92,44 @@ def insert_recipe():
 
 @app.route('/recipes/<recipe_id>/delete', methods=["POST"])
 def delete_recipe(recipe_id):
+    """
+    Deletes the specified recipe from the database and redirects
+    the web application to the recipe list page
+
+    Parameters:
+        recipe_id (str): The id of the recipe to be rendered
+
+    Returns:
+        redirect(url_for("get_recipes")): Redirects to the recipe list page
+    """
     mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
     return redirect(url_for('get_recipes'))
 
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
+    """
+    Renders the web application edit recipe page for the specified recipe
+
+    Parameters:
+        recipe_id (str): The id of the recipe to be rendered
+
+    Returns:
+        render_template("edit_recipe.html", ...): Renders the edit recipe page 
+    """
     return render_template('edit_recipe.html', recipe=mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)}), cuisines=mongo.db.cuisines.find().sort([("name", 1)]))
 
 @app.route('/recipes/<recipe_id>/update', methods=["POST"])
 def update_recipe(recipe_id):
+    """
+    Updates the specified recipe in the database and 
+    redirects the web application to the specified recipe page
+
+    Parameters:
+        recipe_id (str): The id of the recipe to be rendered
+
+    Returns:
+        redirect(url_for('get_recipe', recipe_id=recipe_id)): Redirects to the specified recipe page
+    """
     mongo.db.recipes.update({'_id': ObjectId(recipe_id)}, request.form.to_dict())
     return redirect(url_for('get_recipe', recipe_id=recipe_id))
 
